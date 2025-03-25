fbc807c19ff19734581801f152ca587919ab7829
Dornelas0303 <ritinhadornelas@hotmail.com>
2025-03-25 22:09:44 +0100

    alterando forma de exibicao das notas

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ def ler_arquivo():
     with open("notas.csv","r") as arquivo:
         return arquivo.readlines()
 
-def escrever_arquivo(conteudo): 
+def escrever_arquivo(alunos): 
     with open("notas.csv","a") as arquivo:
-        arquivo.write(conteudo+"\n")
+        arquivo.write(conteudo + "\n")
+        
+
+def exibir_arquivo(alunos):
+    for aluno in alunos:
+        print(f"\nNome: {aluno[0]}")
+        print(f"Nota 1: {aluno[1]}")
+        print(f"Nota 2: {aluno[2]}")
+        print(f"Nota 3: {aluno[3]}")
+        print(f"Nota 4:  {aluno[4]}")
 
 conteudo=ler_arquivo()
-
 conteudo_2=[]
 
 for aluno in conteudo:
@@ -21,10 +29,11 @@ menu='''
 opcao=input(f"Digite uma opcao: {menu}")
 
 if opcao== "1":
-    print(conteudo_2)
+    exibir_arquivo(conteudo_2)
 if opcao== "2":
     novo_aluno=input("Digite o nome do aluno: ")
     nota_1=input("Digite a nota: ")
     nota_2=input("Digite a nota: ")
     nota_3=input("Digite a nota: ")
-    escrever_arquivo(f"{novo_aluno};{nota_1};{nota_2};{nota_3}")
+    nota_4=input("Digite a nota: ")
+    escrever_arquivo(f"{novo_aluno};{nota_1};{nota_2};{nota_3};{nota_4}")
